7454564f93f422b4a49af045e57a197438f7af34
JP MALDONADO <jpmaldonado@jps-imac.lan>
2022-08-18 12:22:54 -0500

    fixed some handlebar bugs

--- Controllers/home-routes.js
+++ Controllers/home-routes.js
@@ -11,23 +11,6 @@ router.get('/', (req, res) => {
   res.render('homepage');
 });
 
-// Route to display static src images
-app.get("/static", (req, res) => {
-  res.render("static");
-});
-
-// Route to display dynamic src images
-app.get("/dynamic", (req, res) => {
-  imageList = [];
-  imageList.push({ src: "T1.jpeg", name: "Taco Combo 1" });
-  imageList.push({ src: "T2.jpeg", name: "Taco Combo 2" });
-  imageList.push({ src: "T3.jpeg", name: "Taco Combo 3" });
-  imageList.push({ src: "T4.jpeg", name: "Taco Combo 4" });
-  imageList.push({ src: "TBTB logo.jpeg", name: "TBTB Logo" });
-  res.render("dynamic", { imageList: imageList });
-});
-
-
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
       res.redirect('/');
